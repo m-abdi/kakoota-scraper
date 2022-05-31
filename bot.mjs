89dc88823bca9941b44d106fef372f5342dce583
Mehdi Abdi <m.abdi.public@gmail.com>
2022-05-31 12:12:52 +0430

    completed

--- bot.mjs
+++ bot.mjs
@@ -1,39 +1,51 @@
 import { giveMeFirstImage } from './searchImage.mjs';
 import { cambridgeData } from './parser.mjs';
 import fetch from 'node-fetch';
-import fs from "fs"
-import "dotenv/config"
-// const words = [{ w: 'programming', l: 'en' }];
-const words = JSON.parse(fs.readFileSync('./all-words.json', 'utf8'));
-let done = []
-async function getData() {
+import fs from 'fs';
+import 'dotenv/config';
+try {
+  const words = JSON.parse(fs.readFileSync('./remained.json', 'utf8'));
+  getData(words);
+} catch {
+  const words = JSON.parse(fs.readFileSync('./all-words.json', 'utf8'));
+  getData(words);
+}
+async function getData(words) {
+  let remained = [...words];
+  
   for (const word of words) {
-    try {
-      const picture_url = await giveMeFirstImage(word);
-      const dictionaryData = await cambridgeData(word);
-      const apiResp = await fetch(process.env.FRONT_URL + '/api/newTest', {
-        headers: { 'Content-Type': 'application/json' },
-        method: 'POST',
-        body: JSON.stringify({
-          picture_url,
-          word: word,
-          data: dictionaryData,
-          language: "en",
-          token: process.env.FRONT_END_API_TOKEN,
-        }),
-      });
-      if (apiResp.ok) {
-        console.log('success');
-        done.push(word)
-      } else {
-        console.log('error');
-      }
-    } catch (e) {
-      console.log('Error on word:', word);
-      console.log(e.message);
-      continue;
+    try{
+
+ const picture_url = await giveMeFirstImage(word);
+    const dictionaryData = await cambridgeData(word);
+    const apiResp = await fetch(process.env.FRONT_URL + '/api/newTest', {
+      headers: { 'Content-Type': 'application/json' },
+      method: 'POST',
+      body: JSON.stringify({
+        picture_url,
+        word: word,
+        data: dictionaryData,
+        language: 'en',
+        token: process.env.FRONT_END_API_TOKEN,
+      }),
+    });
+    if (apiResp.ok) {
+      console.log('success' + ' ' + word);
+      remained = remained.filter((r) => r !== word);
+      fs.writeFile(
+        './remained.json',
+        JSON.stringify(remained),
+        {},
+        (err) => {}
+      );
+    } else {
+      console.log('error');
     }
+    }
+   
+    catch {
+    console.log("error on word:  " + word)
+    continue
+  }
   }
 }
-
-getData();
